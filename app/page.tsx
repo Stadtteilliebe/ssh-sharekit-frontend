6507c4730c911e1f25eefd14a1cfee79f4ceb091
fls-stl <florian@stadtteilliebe.de>
2026-04-15 18:26:46 +0200

    add suspense for page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { SharekitFlow } from "./components/Steps/sharekitFlow";
 
 export default function Page() {
-  return <SharekitFlow />;
+  return (
+    <Suspense fallback={<div>Lade Sharekit …</div>}>
+      <SharekitFlow />
+    </Suspense>
+  );
 }
